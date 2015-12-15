edf8f4ae6a49b6d9d57f227d4b1cbab1ac5fc271
kinoru <xkinoru@gmail.com>
2015-12-15 00:39:33 +0000

    Edit run-before-release script accordingly

--- release.sh
+++ release.sh
@@ -1,2 +1,2 @@
 # run before release.
-strip --strip-all --remove-section=.comment --remove-section=.note ~/.local/bin/u && upx -9 ~/.local/bin/u
+strip --strip-all --remove-section=.comment --remove-section=.note ~/.local/bin/unbreak && upx -9 ~/.local/bin/unbreak
